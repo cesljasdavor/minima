47d04877e951776de590ab5f96e29d6abed923e1
davor1986 <dc48664@fer.hr>
2017-08-04 17:31:34 +0200

    Minor changes to email button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Util} from "./util/util";
 
 @Component({
   selector: 'minima-app',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  isMobile(): boolean {
+    return Util.isMobile();
+  }
 }
